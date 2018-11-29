42f9d91ffd57f5372d7f5a9e5e1ad7e3420a61fc
whitefox200 <42363694+whitefox200@users.noreply.github.com>
2018-11-29 17:49:51 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "2";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "517260943688728597"; // ايدي السررفر
+var channel = "517719579091402762";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },25);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "d") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login("NTE3NzEyNzQ2ODEzMzI1MzUy.DuGNQw.6eAKEghY38SzihEADBY0IXLqnRU");
